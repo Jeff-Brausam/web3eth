defb3adfb21f6f1b519b8bdfeb7cd86ddf70104a
jeff brausam <jeffbrausam@gmail.com>
2021-12-16 15:15:23 -0800

    setting up with github

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,8 @@
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-    solidity: "0.8.10",
-    networks: {
-        hardhat: {
-            chainId: 1337
-        },
-        rinkeby: {
-            url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-            accounts: [`${process.env.RINKEBY_KEY}`],
-        }
-    }
+  solidity: "0.8.10",
 };
