6c6167cba8ebcaae11a32d0f311e1031a035d55a
jeff brausam <jeffbrausam@gmail.com>
2021-12-16 15:48:16 -0800

    feat: deploy script added for helloWorld smart contract

--- test/HelloWorld.ts
+++ test/HelloWorld.ts
@@ -2,8 +2,15 @@ import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 import { expect } from "chai";
 
-describe("hello world", function () {
-  it("should say hi", async function () {
+describe("hello world", () => {
+  it("should say hi", async () => {
+    // Reads the contract (as a json file)
     const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    // Deploys the contract
+    const hello = await HelloWorld.deploy();
+    // Confirms your contract is on the network
+    await hello.deployed();
+
+    expect(await hello.hello()).to.equal("Hello, World");
   });
 });
